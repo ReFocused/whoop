0b9be2f42ad59d1637fa3f7a56df9be0d43fdd8a
Jay Jackson <jj@jj.is-a.dev>
2023-03-13 21:16:20 -0400

    broken but kinda works?

--- src/http.rs
+++ src/http.rs
@@ -1,12 +1,78 @@
 //! A raw HTTP request parser.
 //! We do this manually because we don't need to parse the entire request body.
 
-use circular_buffer::CircularBuffer;
+use std::iter::Peekable;
+
 use heapless::String;
 
-struct Assert<const N: usize>;
-impl<const N: usize> Assert<N> {
-    const LEN_GT_1024: () = assert!(N >= 1024);
+use crate::remove_from_slice;
+
+#[derive(Default, Debug, Clone, Copy)]
+pub enum Protocol {
+    Http,
+    #[default]
+    Https,
+}
+
+impl Protocol {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Protocol::Http => "http",
+            Protocol::Https => "https",
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    /// The protocol is invalid because the path
+    /// doesn't start with `http://` or `https://`.
+    InvalidProtocol,
+    /// The domain or path is too long.
+    /// The maximum length is 32 and 64 respectively.
+    TooLong,
+    /// Missing the path in the path.
+    MissingPath,
+    /// The method is invalid.
+    InvalidMethod,
+    /// The port is invalid.
+    InvalidPort,
+}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::InvalidProtocol => write!(
+                f,
+                "Invalid protocol (the path must start with http:// or https://)"
+            ),
+            Error::TooLong => write!(
+                f,
+                "The domain or path was too long (they have a max of 32 and 64 respectively)"
+            ),
+            Error::MissingPath => write!(
+                f,
+                "Missing the path after the domain (if you want the root path, use /)"
+            ),
+            Error::InvalidMethod => write!(f, "Invalid request method"),
+            Error::InvalidPort => write!(f, "Invalid port"),
+        }
+    }
+}
+impl std::error::Error for Error {}
+
+#[derive(Default, Debug, Clone)]
+pub enum RequestMethod {
+    #[default]
+    Get,
+    Post,
+    Put,
+    Delete,
+    Head,
+    Options,
+    Connect,
+    Trace,
+    Patch,
+    Other(String<8>),
 }
 
 fn skip_line(iter: &mut impl Iterator<Item = u8>) {
@@ -20,124 +86,176 @@ fn skip_line(iter: &mut impl Iterator<Item = u8>) {
 }
 
 #[derive(Default, Debug, Clone)]
-pub enum Protocol {
-    Http,
-    #[default]
-    Https,
-}
-
-#[derive(Debug, Clone)]
-pub enum Error {
-    InvalidProtocol,
-    TooLong,
-}
-
-#[derive(Default, Debug, Clone)]
-#[allow(clippy::large_enum_variant)]
-pub enum State {
-    #[default]
-    New,
-    PastHeader,
-}
-
-#[derive(Default, Debug, Clone)]
 pub struct Parser {
-    state: State,
-    /// The domain of the request, not including the leading `http://`.
-    domain: (Protocol, String<32>),
+    past_header: bool,
+    past_headers: bool,
+    /// The protocol of the request.
+    pub protocol: Protocol,
+    /// The domain of the request.
+    pub addr: (String<32>, u16),
     /// The path of the request, not including the leading `/`.
-    path: String<64>,
+    /// This includes the query string.
+    pub path: String<64>,
+    /// The method of the request.
+    pub method: RequestMethod,
+}
+
+fn copy_slice<const N: usize>(buf: &[u8; N]) -> [u8; N] {
+    let mut new_buf = [0u8; N];
+    unsafe {
+        std::ptr::copy_nonoverlapping(buf.as_ptr(), new_buf.as_mut_ptr(), N);
+    }
+    new_buf
 }
 
 impl Parser {
-    pub fn modify_stream<const N: usize>(
+    pub fn modify_stream<const N: usize>(&mut self, buf: &mut [u8; N]) -> Result<usize, Error> {
+        let mut removed = 0;
+
+        let iter = copy_slice(buf);
+        let mut iter = iter.iter().copied().enumerate().peekable();
+        if !self.past_header {
+            removed += self.parse_header(buf, &mut iter)?;
+            self.past_header = true;
+        }
+
+        Ok(removed)
+    }
+
+    fn parse_header<I: Iterator<Item = (usize, u8)>>(
         &mut self,
-        buf: &mut CircularBuffer<N, u8>,
-    ) -> Result<(), Error> {
-        #[allow(clippy::let_unit_value)]
-        let _ = Assert::<N>::LEN_GT_1024;
+        buf: &mut [u8],
+        iter: &mut Peekable<I>,
+    ) -> Result<usize, Error> {
+        let mut removed = 0;
 
-        let mut iter = buf.iter().copied().enumerate().peekable();
-        match self.state {
-            State::New => {
-                // skip the request method
-                for (_, byte) in iter.by_ref() {
-                    if byte == b' ' {
-                        break;
-                    }
+        self.method = {
+            let mut method = String::new();
+            for (_, byte) in iter.by_ref() {
+                if byte == b' ' {
+                    break;
                 }
-                // get the path
-                assert!(iter.next().map(|(_, s)| s) == Some(b'/'));
-
-                // get the protocol
-                let mut http = "http".bytes();
-
-                let mut protocol = None;
-
-                while let Some(&(_, byte)) = iter.peek() {
-                    let next_byte = http.next();
-                    match next_byte {
-                        Some(b) => {
-                            if b != byte {
-                                return Err(Error::InvalidProtocol);
-                            }
-                            iter.next();
-                        }
-                        None => {
-                            protocol.replace(if byte == b's' {
-                                iter.next();
-                                Protocol::Https
-                            } else {
-                                Protocol::Http
-                            });
-                        }
-                    }
-                }
-
-                // skip the ://
-                iter.next();
-                iter.next();
-                iter.next();
-
-                let protocol = protocol.ok_or(Error::InvalidProtocol)?;
-
-                println!("{:?}", protocol);
-
-                // get the domain
-                let mut domain = String::new();
-                for byte in iter.by_ref() {
-                    if byte == b'/' {
-                        break;
-                    }
-                    domain.push(byte as _).map_err(|_| Error::TooLong)?;
-                }
-                self.domain = (protocol, domain);
-
-                // get the path
-                let mut path = String::new();
-                for byte in iter.by_ref() {
-                    if byte == b' ' {
-                        break;
-                    }
-                    path.push(byte as _).map_err(|_| Error::TooLong)?;
-                }
-                self.path = path;
-
-                skip_line(&mut iter);
-                self.state = State::PastHeader;
+                method.push(byte as _).map_err(|_| Error::InvalidMethod)?;
             }
-            State::PastHeader { .. } => {
-                // Skip the rest of the headers.
-                while let Some(byte) = iter.next() {
-                    if byte == b'\r' {
-                        if let Some(b'\n') = iter.next() {
-                            break;
-                        }
+            match method.as_str() {
+                "GET" => RequestMethod::Get,
+                "POST" => RequestMethod::Post,
+                "PUT" => RequestMethod::Put,
+                "DELETE" => RequestMethod::Delete,
+                "HEAD" => RequestMethod::Head,
+                "OPTIONS" => RequestMethod::Options,
+                "CONNECT" => RequestMethod::Connect,
+                "TRACE" => RequestMethod::Trace,
+                "PATCH" => RequestMethod::Patch,
+                _ => RequestMethod::Other(method),
+            }
+        };
+
+        assert_eq!(iter.next().map(|(_, b)| b as char), Some('/')); // skip the leading `/`
+
+        // get the protocol
+        let mut http = "http".bytes();
+
+        let mut protocol = None;
+        while let Some(&(i, byte)) = iter.peek() {
+            let next_byte = http.next();
+            match next_byte {
+                Some(b) => {
+                    iter.next();
+                    remove_from_slice(buf, i);
+                    if b != byte {
+                        break;
                     }
+                    removed += 1;
+                }
+                None => {
+                    protocol.replace(if byte == b's' {
+                        iter.next();
+                        remove_from_slice(buf, i);
+                        removed += 1;
+                        Protocol::Https
+                    } else {
+                        Protocol::Http
+                    });
+                    break;
                 }
             }
         }
 
-        Ok(())
+        self.protocol = protocol.ok_or(Error::InvalidProtocol)?;
+
+        // skip the ://
+        let Some((i, byte)) = iter.next() else {
+                return Err(Error::InvalidProtocol);
+            };
+        if byte != b':' {
+            return Err(Error::InvalidProtocol);
+        }
+        remove_from_slice(buf, i);
+        removed += 1;
+        let Some((i, byte)) = iter.next() else {
+                return Err(Error::InvalidProtocol);
+            };
+        if byte != b'/' {
+            return Err(Error::InvalidProtocol);
+        }
+        remove_from_slice(buf, i);
+        removed += 1;
+        let Some((i, byte)) = iter.next() else {
+                return Err(Error::InvalidProtocol);
+            };
+        if byte != b'/' {
+            return Err(Error::InvalidProtocol);
+        }
+        remove_from_slice(buf, i);
+        removed += 1;
+
+        // get the domain and strip the domain from the buffer
+        let mut domain = String::new();
+        let mut port = String::<5>::new();
+
+        for (i, byte) in iter.by_ref() {
+            if byte == b'/' {
+                break;
+            } else if byte == b' ' {
+                return Err(Error::MissingPath);
+            } else if byte == b':' {
+                for (i, byte) in iter.by_ref() {
+                    if byte == b'/' {
+                        break;
+                    } else if byte == b' ' {
+                        return Err(Error::MissingPath);
+                    }
+                    port.push(byte as _).map_err(|_| Error::InvalidPort)?;
+                    remove_from_slice(buf, i);
+                    removed += 1;
+                }
+                break;
+            }
+            // TODO: optimize this
+            remove_from_slice(buf, i);
+            removed += 1;
+            domain.push(byte as _).map_err(|_| Error::TooLong)?;
+        }
+        let port = if port.is_empty() {
+            80
+        } else {
+            port.parse().map_err(|_| Error::InvalidPort)?
+        };
+        self.addr = (domain, port);
+
+        // get the path
+        let mut path = String::new();
+        for (_, byte) in iter.by_ref() {
+            if byte == b' ' {
+                break;
+            }
+            path.push(byte as _).map_err(|_| Error::TooLong)?;
+        }
+        self.path = path;
+
+        skip_line(&mut iter.map(|(_, b)| b));
+
+        Ok(removed)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,94 @@
-use circular_buffer::CircularBuffer;
-use may::{go, net::TcpListener};
 use std::{
-    io::{self, Read, Write},
-    net::Ipv4Addr,
+    net::{IpAddr, Ipv4Addr},
+    time::Duration,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    time::timeout,
+};
+use trust_dns_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    TokioAsyncResolver,
 };
 
 mod http;
 
-fn main() {
+async fn handle_error<T, E: std::error::Error>(
+    stream: &mut TcpStream,
+    result: Result<T, E>,
+) -> Result<T, E> {
+    if let Err(ref e) = result {
+        let err = e.to_string();
+        let _ = stream.write_all(format!("HTTP/1.1 500 Internal Server Error\r\nContent-Length: {}\r\nContent-Type: text/plain\r\n\r\n{err}", err.len()).as_bytes()).await;
+    }
+
+    result
+}
+
+macro_rules! stream_loop {
+    ($timeout:expr, $stream:expr, $buf:ident, $n:pat => $body:block) => {
+        loop {
+            let mut $buf = [0u8; 1024];
+            match timeout($timeout, $stream.read(&mut $buf)).await {
+                Err(_) | Ok(Ok(0)) => break, // timeout or EOF
+                Ok(Ok($n)) => $body,
+                Ok(Err(e)) => panic!("Error reading from stream: {}", e),
+            };
+        }
+    };
+}
+
+#[tokio::main]
+async fn main() {
     let port = std::env::var("PORT").map_or(8000, |p| p.parse().unwrap());
 
     let addr = Ipv4Addr::new(127, 0, 0, 1);
-    let listener = TcpListener::bind((Ipv4Addr::new(127, 0, 0, 1), port)).unwrap();
+    let listener = TcpListener::bind((Ipv4Addr::new(127, 0, 0, 1), port))
+        .await
+        .unwrap();
+
     println!("Listening at http://{addr}:{port}");
 
-    while let Ok((mut stream, _)) = listener.accept() {
-        go!(move || {
-            stream
-                .set_read_timeout(Some(std::time::Duration::from_secs(10)))
-                .unwrap();
-            stream
-                .set_write_timeout(Some(std::time::Duration::from_secs(10)))
-                .unwrap();
-
-            let mut buf = CircularBuffer::<2048, u8>::new();
-
+    while let Ok((mut stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
             let mut parser = http::Parser::default();
 
-            loop {
-                let mut tmp = [0u8; 1024];
-                match dbg!(stream.read(&mut tmp)) {
-                    Ok(0) => break,
-                    Ok(n) => {
-                        buf.extend_from_slice(unsafe {
-                            // SAFETY: `n` is the number of bytes pushed into the buffer.
-                            tmp.get_unchecked(..n)
-                        });
-                        parser.modify_stream(&mut buf).unwrap();
-                        println!("{parser:?}");
-                    }
-                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
-                        buf.clear();
-                    }
-                    Err(e) => {
-                        println!("Error: {e}");
-                        break;
-                    }
+            stream_loop!(Duration::from_secs(10), stream, buf, n => {
+                let removed = handle_error(&mut stream, parser.modify_stream(&mut buf)).await.unwrap();
+                let ip = if let Ok(ip) = parser.addr.0.parse::<IpAddr>() {
+                    ip
+                } else {
+                    TokioAsyncResolver::tokio(ResolverConfig::cloudflare(), ResolverOpts::default())
+                        .unwrap()
+                        .lookup_ip(&*parser.addr.0)
+                        .await
+                        .unwrap()
+                        .iter()
+                        .next()
+                        .unwrap()
                 };
-                buf.extend_from_slice(&tmp);
-            }
+                let mut conn_stream = TcpStream::connect((ip, parser.addr.1)).await.unwrap();
+                std::fs::write("test.http", &buf[..n - removed]).unwrap();
+                conn_stream.write_all(&buf[..n - removed]).await.unwrap();
+                stream_loop!(Duration::from_secs(5), conn_stream, buf, _ => {
+                    stream.write_all(&buf).await.unwrap();
+                });
+            });
         });
+    }
+}
+
+/// Removes an element from a slice.
+///
+/// Runs in O(n) time.
+fn remove_from_slice(slice: &mut [u8], index: usize) {
+    let len = slice.len();
+    if index < len {
+        unsafe {
+            let ptr = slice.as_mut_ptr().add(index);
+            std::ptr::copy(ptr.add(1), ptr, len - index - 1);
+            std::ptr::write_bytes(ptr.add(len - index - 1), 0, 1);
+        }
     }
 }
